fdad4978bff9f159a6d39612349a18239d5efa80
Espero AKPOLI <mongodbna@gmail.com>
2024-01-06 16:17:51 +0100

    update

--- blog/models/Category.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Category(models.Model):
-    title = models.CharField(null=True)
-    description = models.CharField(null=True)
-    content = models.CharField(null=True)
